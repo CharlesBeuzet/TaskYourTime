195088fb6f20dddbe9690db16e468d674193dec6
Loan <loanplichon78440@gmail.com>
2021-11-13 19:46:19 -0500

    XML modification of Authentication pages and implementation of the UserService.kt in the functionalities

--- app/src/main/java/com/example/taskyourtime/LogInActivity.kt
+++ app/src/main/java/com/example/taskyourtime/LogInActivity.kt
@@ -10,11 +10,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.taskyourtime.model.User
 import com.example.taskyourtime.note.ListNoteActivity
+import com.example.taskyourtime.services.UserService
 import com.example.taskyourtime.services.appModule
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -25,7 +28,10 @@ import org.koin.core.context.startKoin
     lateinit var subscription_link : TextView
     lateinit var email_text : EditText
     lateinit var password_text : EditText
+    lateinit var login_error_text: TextView
     private lateinit var auth: FirebaseAuth
+
+    private val userService by inject<UserService>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +51,7 @@ import org.koin.core.context.startKoin
         subscription_link = findViewById(R.id.subscription_link)
         email_text = findViewById(R.id.email)
         password_text = findViewById(R.id.password)
+        login_error_text = findViewById(R.id.loginError)
 
         login_button.setOnClickListener(listener)
         subscription_link.setOnClickListener(listener)
@@ -53,29 +60,18 @@ import org.koin.core.context.startKoin
     val listener = View.OnClickListener { view ->
         when(view.getId()) {
             R.id.button_login -> {
-                //Get the data and send it to Firebase for check and connexion
-                val email = email_text.text.toString()
-                val password = password_text.text.toString()
-                auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success")
-                            val user = auth.currentUser
-                            val intent = Intent(this, DefaultActivity::class.java)
-                            //val intent = Intent(this, ListNoteActivity::class.java)
-                            startActivity(intent)
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.exception)
-                            Toast.makeText(baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
-                        }
+                userService.loginUser(email_text.text.toString(), password_text.text.toString()).observeForever{
+                    success ->
+                    if(success == true){
+                        val intent = Intent(this, DefaultActivity::class.java)
+                        startActivity(intent)
                     }
-
+                    else{
+                        login_error_text.setText(R.string.login_error)
+                    }
+                }
             }
             R.id.subscription_link -> {
-                //Launch of the subscribtion activity
                 val intent = Intent(this, SubscriptionActivity::class.java)
                 startActivity(intent)
                 Log.d("CLICK","Activity to subscribe launched")
--- app/src/main/java/com/example/taskyourtime/SubscriptionActivity.kt
+++ app/src/main/java/com/example/taskyourtime/SubscriptionActivity.kt
@@ -1,15 +1,14 @@
 package com.example.taskyourtime
 
-import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import com.example.taskyourtime.services.NoteService
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.taskyourtime.services.UserService
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -23,6 +22,9 @@ class SubscriptionActivity : AppCompatActivity() {
     lateinit var password_text : EditText
     lateinit var firstname_text : EditText
     lateinit var lastname_text : EditText
+    lateinit var error_text: TextView
+
+    private val userService by inject<UserService>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,40 +32,42 @@ class SubscriptionActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        subscribe_button = findViewById(R.id.button_subscribe)
-        email_text = findViewById(R.id.email)
-        password_text = findViewById(R.id.password)
-        firstname_text = findViewById(R.id.firstname)
-        lastname_text = findViewById(R.id.lastname)
+        subscribe_button = findViewById(R.id.btnSignUp)
+        email_text = findViewById(R.id.etEmail)
+        password_text = findViewById(R.id.etPassword)
+        firstname_text = findViewById(R.id.etFirstName)
+        lastname_text = findViewById(R.id.etLastName)
+        error_text = findViewById(R.id.errorTV)
 
         subscribe_button.setOnClickListener(listener)
     }
 
     val listener = View.OnClickListener { view ->
         when(view.getId()) {
-            R.id.button_subscribe -> {
+            R.id.btnSignUp -> {
                 //Get the data and store it in Firebase
                 val email = email_text.text.toString()
                 val password = password_text.text.toString()
-                Log.d("AUTH","Email : " + email + "password : " + password)
+                val firstName = firstname_text.text.toString()
+                val lastName = lastname_text.text.toString()
 
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success")
-                            val user = auth.currentUser
+                if(email == "" || password == "" || firstName == "" || lastName == ""){
+                    error_text.text = getString(R.string.sign_up_error)
+                }
+                else {
+                    Log.d("AUTH", "Email : " + email + "password : " + password)
+
+                    (userService.registerUser(firstName, lastName, email, password, "resources/picture" )).observeForever{
+                        success ->
+                        if(success == true){
                             val intent = Intent(this, DefaultActivity::class.java)
                             startActivity(intent)
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                            Toast.makeText(baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
-                            //updateUI(null)
+                        }
+                        else{
+                            error_text.setText(R.string.sign_up_content_error)
                         }
                     }
-
+                }
             }
         }
     }
