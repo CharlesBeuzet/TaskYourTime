378a28e796d9082d50c7185ebd1f3faa9bab1943
Loan <loanplichon78440@gmail.com>
2021-11-21 20:30:05 -0500

    Calendar front end almost done (lack of event modification functionality)

--- app/src/main/java/com/example/taskyourtime/calendar/CalendarViewActivity.kt
+++ app/src/main/java/com/example/taskyourtime/calendar/CalendarViewActivity.kt
@@ -1,8 +1,11 @@
 package com.example.taskyourtime.calendar
 
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +16,7 @@ import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.taskyourtime.R
 import com.example.taskyourtime.databinding.ActivityCalendarViewBinding
 import com.example.taskyourtime.model.CalendarEvent
 import com.example.taskyourtime.services.CalendarService
@@ -24,7 +28,9 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.delay
 import org.koin.android.ext.android.inject
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -52,13 +58,6 @@ class CalendarViewActivity : Fragment() {
     ): View? {
         binding = ActivityCalendarViewBinding.inflate(inflater, container, false)
         val view = _binding.root
-        val calendarView = binding?.calendarView
-        calendarView?.setOnDateChangeListener{ _, year, month, dayOfMonth ->
-            // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
-            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
-            selectedDate = LocalDate.of(year, month, dayOfMonth)
-            Log.d(TAG, msg)
-        }
         return view
     }
 
@@ -96,24 +95,22 @@ class CalendarViewActivity : Fragment() {
         Log.d(TAG, "CalendarViewCreated")
         super.onViewCreated(view, savedInstanceState)
         Log.d("Event creation", "Inside the createview of events")
+
+        binding?.tvDayInfo?.text = String.format(getString(R.string.info_day), selectedDate.dayOfWeek.toString().substring(0,1) + selectedDate.dayOfWeek.toString().substring(1).toLowerCase(), selectedDate.dayOfMonth.toString(), selectedDate.month.toString().substring(0,1) + selectedDate.month.toString().substring(1).toLowerCase(), selectedDate.year.toString())
+
         binding?.addEventButton?.setOnClickListener{
             Log.d("CLICK", "Adding event")
             val intentAddCalendarEvent = Intent(context, AddToCalendarActivity::class.java)
             startActivity(intentAddCalendarEvent)
-
         }
 
-        database = Firebase.database.reference.child("calendarEvents")
-        val childEventListener = object : ChildEventListener {
+        var childEventListener = object : ChildEventListener {
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                Log.d(TAG, "onChildAdded::" + snapshot.key!!)
                 val map = snapshot.value as Map<String?, Any?>
                 val myEvent = CalendarEvent(map)
                 myEvent.id = snapshot.key!!
-
-                myEvent.name?.let { Log.d(TAG, it) }
-                if (Firebase.auth.uid == myEvent.user_id && myEvent.begin_date?.split(" ")?.get(0) == selectedDate.format(
-                        DateTimeFormatter.ofPattern("dd/MM/yy")).toString()) {
+                if(myEvent.begin_date?.split(" ")?.get(0) == selectedDate.format(DateTimeFormatter.ofPattern("dd/MM/yy")).toString()){
+                    Log.d(TAG, "onChildAdded::" + snapshot.key!!)
                     calendarEvents.add(myEvent)
                 }
                 binding?.recyclerView?.adapter?.notifyDataSetChanged()
@@ -150,9 +147,28 @@ class CalendarViewActivity : Fragment() {
             override fun onCancelled(error: DatabaseError) {
                 Log.w(TAG, "postComments:onCancelled", error.toException())
             }
+
         }
+
+        binding?.calendarView?.setOnDateChangeListener{ _, year, month, dayOfMonth ->
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            selectedDate = LocalDate.of(year, month+1, dayOfMonth)
+            Log.d(TAG, msg)
+
+            binding?.tvDayInfo?.text = String.format(getString(R.string.info_day), selectedDate.dayOfWeek.toString().substring(0,1) + selectedDate.dayOfWeek.toString().substring(1).toLowerCase(), selectedDate.dayOfMonth.toString(), selectedDate.month.toString().substring(0,1) + selectedDate.month.toString().substring(1).toLowerCase(), selectedDate.year.toString())
+
+            calendarEvents.clear()
+            database = Firebase.database.reference.child("calendarEvents")
+            database.removeEventListener(childEventListener)
+            database.addChildEventListener(childEventListener)
+
+            binding?.recyclerView?.adapter?.notifyDataSetChanged()
+        }
+
+        database = Firebase.database.reference.child("calendarEvents")
         database.addChildEventListener(childEventListener)
 
         displayEvents()
+
     }
 }
--- app/src/main/java/com/example/taskyourtime/calendar/CalendarViewAdapter.kt
+++ app/src/main/java/com/example/taskyourtime/calendar/CalendarViewAdapter.kt
@@ -7,11 +7,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.taskyourtime.databinding.CalendarEventCellBinding
 import com.example.taskyourtime.model.CalendarEvent
 import com.example.taskyourtime.services.CalendarService
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 private lateinit var binding: CalendarEventCellBinding
 
 class CalendarViewAdapter(
-        private val data: MutableList<CalendarEvent>,
+        private var data: MutableList<CalendarEvent>,
         private val calendarService: CalendarService,
 ) : RecyclerView.Adapter<CalendarViewAdapter.CalendarViewHolder>(){
 
@@ -23,6 +27,7 @@ class CalendarViewAdapter(
 
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
         val data: CalendarEvent = data[position]
+        Log.d("onBindViewHolder", "${data.name}" +" "+ "${data.begin_date}")
         holder.nameEvent.text = data.name
         holder.descriptionEvent.text = data.description
         holder.beginDateTimeEvent.text = data.begin_date
