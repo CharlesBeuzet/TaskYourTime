98ad974e9d96b3c443ee522bd30c3e2c302368b0
SeanAnica <37900784+SeanAnica@users.noreply.github.com>
2021-11-25 09:00:28 -0500

    Update VisualizeNoteActivity.kt
    finalisation de la mise à jour des notes dans la visualisation

--- app/src/main/java/com/example/taskyourtime/note/VisualizeNoteActivity.kt
+++ app/src/main/java/com/example/taskyourtime/note/VisualizeNoteActivity.kt
@@ -7,17 +7,44 @@ import android.text.Html
 import android.util.Log
 import com.example.taskyourtime.databinding.ActivityVisualizeNoteBinding
 import com.example.taskyourtime.model.Note
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class VisualizeNoteActivity : AppCompatActivity() {
     private var TAG = "VisualizeNoteActivity"
     private lateinit var binding : ActivityVisualizeNoteBinding
     private lateinit var note: Note
+    private lateinit var database: DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVisualizeNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
         note = intent.getSerializableExtra("noteClicked") as Note;
+
+        database = Firebase.database.reference.child("notes").child(note.id.toString())
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d(TAG, "onChildChanged:" + snapshot.key!!)
+                if(note.id == snapshot.key!!){
+                    val map = snapshot.value as Map<String?, Any?>
+                    note = Note(map)
+                    note.id = snapshot.key!!
+                    binding.visualNoteName.text = Html.fromHtml(note.name, Html.FROM_HTML_MODE_LEGACY)
+                    binding.visualNoteContent.text = Html.fromHtml(note.content, Html.FROM_HTML_MODE_LEGACY)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d(TAG, "Erreur inconnue.")
+            }
+
+        }
+        database.addValueEventListener(valueEventListener)
+
+
         binding.visualNoteName.text = Html.fromHtml(note.name, Html.FROM_HTML_MODE_LEGACY)
         binding.visualNoteContent.text = Html.fromHtml(note.content, Html.FROM_HTML_MODE_LEGACY)
 
