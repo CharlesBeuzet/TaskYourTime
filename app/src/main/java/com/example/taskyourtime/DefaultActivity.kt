fae44951c3f89243697d27e0009f0f2f76fe6d6c
SeanAnica <37900784+SeanAnica@users.noreply.github.com>
2021-12-01 01:52:47 -0500

    Update DefaultActivity.kt

--- app/src/main/java/com/example/taskyourtime/DefaultActivity.kt
+++ app/src/main/java/com/example/taskyourtime/DefaultActivity.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.taskyourtime.calendar.CalendarViewActivity
+import com.example.taskyourtime.group.ListGroupActivity
 import com.example.taskyourtime.note.ListNoteActivity
 import com.example.taskyourtime.productivity.ProductivityActivity
 import com.example.taskyourtime.todolist.ToDoListActivity
@@ -63,7 +64,7 @@ class DefaultActivity : FragmentActivity() {
                 1 -> view = ListNoteActivity()
                 2 ->  view = ToDoListActivity()
                 3 -> view = ProductivityActivity()
-                4 -> view = DefaultFragment()
+                4 -> view = ListGroupActivity()
                 else -> view = DefaultFragment()
             }
             return view
