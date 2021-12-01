952bf4fb0888f2cf2a05a8735fa870a79a1e7d10
SeanAnica <37900784+SeanAnica@users.noreply.github.com>
2021-12-01 01:46:22 -0500

    affichage de la liste de groupes
    à tester :alarm_clock:
    suppression de fichiers inutiles

--- app/src/main/java/com/example/taskyourtime/group/ListGroupActivity.kt
+++ app/src/main/java/com/example/taskyourtime/group/ListGroupActivity.kt
@@ -1,14 +1,142 @@
 package com.example.taskyourtime.group
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.taskyourtime.databinding.ActivityListGroupBinding
+import com.example.taskyourtime.model.Group
+import com.example.taskyourtime.services.GroupService
+import com.example.taskyourtime.services.UserService
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import org.koin.android.ext.android.inject
 
 
-class ListGroupActivity : AppCompatActivity() {
+class ListGroupActivity : Fragment(), ListGroupAdapter.OnItemClickListener{
 
+    private var binding : ActivityListGroupBinding? = null
+    private val groupService by inject<GroupService>()
+    private val _binding get() = binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val TAG = "ListGroupActivity"
+    private lateinit var database: DatabaseReference
+    private val groups: MutableList<Group> = mutableListOf<Group>()
+    private val userService by inject<UserService>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ActivityListGroupBinding.inflate(inflater, container, false)
+        val view = _binding.root
+        return view
     }
+
+    private fun displayGroups(){
+        Log.d(TAG, "displayGroups")
+        binding?.recyclerView?.layoutManager = LinearLayoutManager(context)
+        binding?.recyclerView?.adapter = ListGroupAdapter(groups, groupService, this)
+        binding?.loaderFeed?.isVisible = false
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.i(TAG, "ACTIVITY SOPPED")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "ACTIVITY DESTROYED")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"Fragment started")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "ListGroupView created")
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("Group creation", "Inside the createview of groups")
+        binding?.adGroupButton?.setOnClickListener{
+            Log.d("Click", "Adding group")
+            /*val intentAddGroup = Intent(context, AddGroupActivity::class.java)
+            startActivity(intentAddGroup)
+             */
+        }
+
+        database = Firebase.database.reference.child("groups")
+        val childEventListener = object : ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildAdded::" + snapshot.key!!)
+                val map = snapshot.value as Map<String?, Any?>
+                val myGroup = Group(map)
+                myGroup.id = snapshot.key!!
+                //faire ce qu'on veut faire : afficher le groupe dans le recyclerView
+                myGroup.name?.let { Log.d(TAG, it) }
+                if(Firebase.auth.uid == myGroup.ownerId || myGroup.userIdList.contains(Firebase.auth.uid)){
+                    groups.add(myGroup)
+                }
+                binding?.recyclerView?.adapter?.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val index = groups.indexOfFirst {it.id == snapshot.key!!}
+                if(index >= 0 && (groups[index].ownerId == Firebase.auth.uid || groups[index].userIdList.contains(Firebase.auth.uid))){
+                    val map = snapshot.value as Map<String?, Any?>
+                    val myGroup = Group(map)
+                    val groupId = groups[index].id
+                    binding?.recyclerView?.adapter?.notifyDataSetChanged()
+                }
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + snapshot.key!!)
+                val index = groups.indexOfFirst { it.id == snapshot.key!! }
+                if(index >= 0 && (groups[index].ownerId == Firebase.auth.uid || groups[index].userIdList.contains(Firebase.auth.uid))){
+                    val map = snapshot.value as Map<String?, Any?>
+                    groups.removeAt(index)
+                    binding?.recyclerView?.adapter?.notifyItemRemoved(index)
+                    Toast.makeText(binding?.root?.context, "Groupe supprimé", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildMoved:" + snapshot.key!!)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "onCancelled", error.toException())
+            }
+        }
+        database.addChildEventListener(childEventListener)
+        displayGroups()
+    }
+
+    override fun onItemClick(position: Int){
+        val clikedItem = groups[position]
+        /*val intentVisualizeGroup = Intent(context, VisualizeGroupActivity::class.java)
+        intentVisualizeGroup.putExtra("groupClicked",clikedItem)
+        startActivity(intentVisualizeGroup)*/
+        Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
---
+++ app/src/main/java/com/example/taskyourtime/group/ListGroupAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.taskyourtime.group
+
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskyourtime.databinding.GroupItemCellBinding
+import com.example.taskyourtime.model.Group
+import com.example.taskyourtime.note.ListNoteAdapter
+import com.example.taskyourtime.services.GroupService
+
+private lateinit var binding: GroupItemCellBinding
+
+
+class ListGroupAdapter(
+    private val data: MutableList<Group>,
+    private val noteService: GroupService,
+    private val listener: ListGroupActivity,
+): RecyclerView.Adapter<ListGroupAdapter.ListGroupHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListGroupHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        binding = GroupItemCellBinding.inflate(inflater, parent, false)
+        return ListGroupHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListGroupHolder, position: Int) {
+        val data: Group = data[position]
+        holder.groupName.text = Html.fromHtml(data.name, Html.FROM_HTML_MODE_LEGACY)
+        holder.groupOwner.text = Html.fromHtml(data.ownerId,Html.FROM_HTML_MODE_LEGACY)
+        val idGroup = data.id.toString()
+        /*holder.deleteGroupButton.setOnClickListener{
+            Log.d("Asupprimer","Groupe dont l'id est id a été supprimée")
+            noteService.deleteNote(idNote)
+        }*/
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class ListGroupHolder(binding: GroupItemCellBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener{
+        val groupName = binding.groupName
+        val groupOwner = binding.groupOwner
+        val layout = binding.root
+
+        init{
+            binding.root.setOnClickListener(this)
+        }
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if(position != RecyclerView.NO_POSITION)
+            {
+                listener.onItemClick(position)
+            }
+        }
+    }
+    interface OnItemClickListener{
+        fun onItemClick(position: Int)
+    }
+}
